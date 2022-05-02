eeca7296ae788140a84e5a729aa3e652c63d6569
KitsanaMameesukh <mameesukh79@gmail.com>
2022-05-02 22:52:56 +0700

    update

--- sample3.py
+++ sample3.py
@@ -1,11 +1,14 @@
 def find_MaxIndex(num):
   maxValue = 0
   index = 0
+  indexmax = 0
   for i in num:
     if i>maxValue:
       maxValue = i
-      index+=1
-  print("ลำดับที่มีค่ามากที่สุด คือ index = "+str(index))
+      indexmax = index
+    index+=1
+    
+  print("ลำดับที่มีค่ามากที่สุด คือ index = "+str(indexmax))
   
 if __name__ == '__main__':
-  result = find_MaxIndex([1,2,1,3,5,6,4])
+  result = find_MaxIndex([1,2,7,3,5,6,4])
